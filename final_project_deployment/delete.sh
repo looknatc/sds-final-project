4d40417c70558eef6de016a1b2464a46c4026a3e
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-11-13 16:32:13 +0700

    fix: add start and delete script

--- final_project_deployment/delete.sh
+++ final_project_deployment/delete.sh
@@ -1,6 +1,8 @@
-microk8s.kubectl delete deployment frontend-deployment 
-
-microk8s.kubectl delete services frontend-service
-microk8s.kubectl delete services calculation-service
-microk8s.kubectl delete services classification-service
-microk8s.kubectl delete services risk-service
+microk8s.kubectl delete deployment risk-deployment
+microk8s.kubectl delete deployment frontend-deployment
+microk8s.kubectl delete deployment classification-deployment
+microk8s.kubectl delete deployment calculation-deployment
+microk8s.kubectl delete service frontend-service
+microk8s.kubectl delete service calculation-service
+microk8s.kubectl delete service classification-service
+microk8s.kubectl delete service risk-service
--- final_project_deployment/start.sh
+++ final_project_deployment/start.sh
@@ -1,6 +1,8 @@
+microk8s.kubectl apply -f classification-deployment.yaml
+microk8s.kubectl apply -f calculation-deployment.yaml
+microk8s.kubectl apply -f risk-deployment.yaml
 microk8s.kubectl apply -f frontend-deployment.yaml
 microk8s.kubectl apply -f frontend-service.yaml
 microk8s.kubectl apply -f calculation-service.yaml
 microk8s.kubectl apply -f classification-service.yaml
 microk8s.kubectl apply -f risk-service.yaml
-
